fe3d53af9195522776d68af26816fd3404778da4
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-12-15 18:05:27 -0300

    passos iniciais

--- netflix/app/Models/User.php
+++ netflix/app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Jamesh\Uuid\HasUuid;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUuid;
 
     /**
      * The attributes that are mass assignable.
