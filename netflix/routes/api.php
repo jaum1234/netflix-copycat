9eada87f539834d8bfece5d1a6cb3c067d7da544
Pedro Leopoldo <pedro.lepoldo@estudante.ufjf.br>
2021-12-15 19:23:21 -0300

    Criando os testes para criacao de usuarios

--- netflix/routes/api.php
+++ netflix/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('/users', UserController::class)->names('users');
+Route::resource('/categories', CategoryController::class)->names('categories');
+Route::resource('/users', UserController::class)->names('users');
+Route::resource('/videos', VideoController::class)->names('videos');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
